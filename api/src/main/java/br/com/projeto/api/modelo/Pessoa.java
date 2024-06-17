6ff8dad4c22917bc142ea463229a6071fa3348e1
2002Larissa <larissadssantos1515@gmail.com>
2024-06-17 13:51:48 -0300

    Get e Set

---
+++ api/src/main/java/br/com/projeto/api/modelo/Pessoa.java
@@ -0,0 +1,22 @@
+package br.com.projeto.api.modelo;
+
+public class Pessoa {
+    
+    //Atributos
+    private String nome;
+    private int idade;
+
+    //Get e Set
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
