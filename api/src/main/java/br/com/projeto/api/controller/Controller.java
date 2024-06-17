67bc0a0cfa8260fec4a7138655d21bc009c8ff13
2002Larissa <larissadssantos1515@gmail.com>
2024-06-17 15:42:02 -0300

    Autowired

--- api/src/main/java/br/com/projeto/api/controller/Controller.java
+++ api/src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,5 +1,6 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.modelo.Pessoa;
+import br.com.projeto.api.repositorio.Repositorio;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    private Repositorio acao;
     
     @GetMapping("")
     public String mensagem(){
