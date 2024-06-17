d3e3419e9e19c08a26ff4c7bcd34527020e10eec
2002Larissa <larissadssantos1515@gmail.com>
2024-06-17 13:46:01 -0300

    PathVariable

--- api/src/main/java/br/com/projeto/api/ApiApplication.java
+++ api/src/main/java/br/com/projeto/api/ApiApplication.java
@@ -2,6 +2,7 @@ package br.com.projeto.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ApiApplication {
--- api/src/main/java/br/com/projeto/api/controller/Controller.java
+++ api/src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,6 +1,7 @@
 package br.com.projeto.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,15 @@ public class Controller {
     public String mensagem(){
         return("Hello world!");
     }
+
+    @GetMapping("/boas-vindas")
+	public String boasvindas(){
+		return "Seja bem-vindo!";
+	}
+    
+	@GetMapping("/boas-vindas/{nome}")
+	public String boasvindas(@PathVariable String nome){
+		return "Seja bem-vindo!" + nome;
+	}
+
 }
