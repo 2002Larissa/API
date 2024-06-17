30e7570e5d5b3b90222b50c2388654b9160e8e2a
2002Larissa <larissadssantos1515@gmail.com>
2024-06-17 14:05:47 -0300

    Teste com thunder client

--- api/src/main/java/br/com/projeto/api/controller/Controller.java
+++ api/src/main/java/br/com/projeto/api/controller/Controller.java
@@ -2,7 +2,11 @@ package br.com.projeto.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.modelo.Pessoa;
 
 @RestController
 public class Controller {
@@ -21,5 +25,10 @@ public class Controller {
 	public String boasvindas(@PathVariable String nome){
 		return "Seja bem-vindo!" + nome;
 	}
+
+    @PostMapping("/pessoa")
+    public Pessoa pessoa(@RequestBody Pessoa p){
+        return p;
+    }
 
 }
