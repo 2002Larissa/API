09d7f96b06decdcffc2bcea97a3820450fccdc6c
2002Larissa <larissadssantos1515@gmail.com>
2024-06-20 14:05:07 -0300

    Response Entity

--- api/src/main/java/br/com/projeto/api/controller/Controller.java
+++ api/src/main/java/br/com/projeto/api/controller/Controller.java
@@ -3,6 +3,8 @@ package br.com.projeto.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -119,6 +121,11 @@ public class Controller {
     @PostMapping("/pessoa")
     public Pessoa pessoa(@RequestBody Pessoa p){
         return p;
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<?> status(){
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 }
