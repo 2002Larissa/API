eca24fc0c51bdb30ca273b85d2328793959706f7
2002Larissa <larissadssantos1515@gmail.com>
2024-06-20 14:36:16 -0300

    Annotation @Component

--- api/src/main/java/br/com/projeto/api/servico/Servico.java
+++ api/src/main/java/br/com/projeto/api/servico/Servico.java
@@ -1,8 +1,13 @@
 package br.com.projeto.api.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.projeto.api.modelo.Mensagem;
 
 @Service
 public class Servico {
     
+    @Autowired
+    private Mensagem mensagem;
 }
